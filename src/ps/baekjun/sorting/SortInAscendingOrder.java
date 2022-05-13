1729df2c084dadcfee946865575837b9df271380
banghs <hsbang.thom@gmail.com>
2022-05-13 23:29:30 +0900

    boj sort 2751

---
+++ src/ps/baekjun/sorting/SortInAscendingOrder.java
@@ -0,0 +1,32 @@
+package ps.baekjun.sorting;
+
+//silver 5, 2751
+// parallel sort(merge), quick sort is not allowed here (timeout)
+public class SortInAscendingOrder {
+
+    public static int[] arr = {5,3,4,1,2,3};
+
+    public static void main(String[] args) {
+
+        // heap sort
+
+        // build max heap from data
+
+        // exchange root with the last leaf node
+
+        // heapify again
+
+        // iterate to 1 index
+
+
+    }
+
+    private static void buildHeapTree(int[] data, int index) {
+
+        // if there is no child node , then finish
+
+        // if there is left node,
+
+
+    }
+}
