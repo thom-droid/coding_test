180a76d7bc2ff5d0773ff86c6955ed5ca13f606b
thom-window <hsbang.thom@gmail.com>
2022-05-22 15:48:25 +0900

    ps boj sort 11652 done

--- src/ps/baekjun/sorting/Sort11652.java
+++ src/ps/baekjun/sorting/Sort11652.java
@@ -3,19 +3,17 @@ package ps.baekjun.sorting;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 //Silver 4
 public class Sort11652 {
 
     private static int getLargestNumber(HashMap<Long, Integer> hashMap) {
 
-        long largestNumber = 0;
-        hashMap.entrySet().stream().max(Comparator.comparingInt(Map.Entry::getValue));
+        return Collections.max(hashMap.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getValue();
 
     }
     public static void main(String[] args) throws IOException {
@@ -28,5 +26,7 @@ public class Sort11652 {
             int value = map.getOrDefault(key, 0);
             map.put(key, ++value);
         }
+        int result = getLargestNumber(map);
+        System.out.println(result);
     }
 }
