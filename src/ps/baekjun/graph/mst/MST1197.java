2f69814770a4cabfab101618ddfb967af54f9303
thom <thom-droid@github.com>
2022-06-28 23:55:46 +0900

    working on

---
+++ src/ps/baekjun/graph/mst/MST1197.java
@@ -0,0 +1,18 @@
+package ps.baekjun.graph.mst;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class MST1197 {
+
+    //Kruskal's algorithm
+
+    public static int kruskal() {
+
+    }
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
