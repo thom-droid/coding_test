3d44d80a3c9d45798a3c34dc93ca5570b69ca47c
thom-window <hsbang.thom@gmail.com>
2022-08-03 18:10:13 +0900

    ps leet wokring on

--- src/ps/leet/MergeTwoSortedList.java
+++ src/ps/leet/MergeTwoSortedList.java
@@ -15,14 +15,18 @@ public class MergeTwoSortedList {
      * }
      */
     public static void main(String[] args) {
-        ListNode list1 = new ListNode();
-        ListNode list2 = new ListNode();
+        ListNode list1 = new ListNode(-9);
+        list1.next = new ListNode(3);
+        ListNode list2 = new ListNode(5);
+        list2.next = new ListNode(7);
 //        ListNode list1 = new ListNode(1);
 //        list1.next = new ListNode(2);
 //        list1.next.next = new ListNode(4);
 //        ListNode list2 = new ListNode(1);
 //        list2.next = new ListNode(3);
 //        list2.next.next = new ListNode(4);
+
+
 
         ListNode result = mergeTwoLists(list1, list2);
 
@@ -32,6 +36,10 @@ public class MergeTwoSortedList {
         }
     }
     public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        if (list1 == null && list2 == null) {
+            return null;
+        }
 
         ListNode head = new ListNode();
         ListNode result;
@@ -58,12 +66,14 @@ public class MergeTwoSortedList {
 
         while (list1 != null) {
             head.val = list1.val;
+            head.next = new ListNode();
             head = head.next;
             list1 = list1.next;
         }
 
         while (list2 != null) {
             head.val = list2.val;
+            head.next = new ListNode();
             head = head.next;
             list2 = list2.next;
         }
