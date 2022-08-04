bcbc5bfbff5d48f16243c2716977fbc61838950c
thom-window <hsbang.thom@gmail.com>
2022-08-04 10:31:55 +0900

    ps(boj-graph): 2667 working on

---
+++ src/ps/baekjun/graph/Graph2667.java
@@ -0,0 +1,99 @@
+package ps.baekjun.graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+//silver 1
+public class Graph2667 {
+
+    static int N;
+    static int[][] graph;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        graph = new int[N][N];
+        visited = new boolean[N][N];
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+
+            for (int j = 0; j < N; j++) {
+                graph[i][j] = Integer.parseInt(String.valueOf(s.charAt(j)));
+            }
+        }
+
+        int buildingNumber = 1;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int count = 0;
+                LinkedList<Node> queue = new LinkedList<>();
+                Node node = new Node(i, j);
+                queue.push(node);
+                count = bfs(queue, buildingNumber);
+
+                if (count > 0) {
+                    sb.append(count).append('\n');
+                    buildingNumber++;
+                }
+            }
+        }
+
+        Arrays.stream(graph).forEach(x -> System.out.println(Arrays.toString(x)));
+    }
+
+    private static int bfs(LinkedList<Node> queue, int buildingNumber) {
+        int count = 0;
+
+        while (queue.size() != 0) {
+            Node next = queue.poll();
+            int x = next.x;
+            int y = next.y;
+
+            if (graph[x][y] == 1 && !visited[x][y]) {
+                visited[x][y] = true;
+                graph[x][y] = buildingNumber;
+                count++;
+            }
+
+            for (int i = 0; i < dx.length; i++) {
+                int xValue = x + dx[i];
+                int yValue = y + dy[i];
+
+                // check if outside the board
+                if (xValue >= N || yValue >= N || xValue < 0 || yValue < 0) {
+                    continue;
+                }
+
+                if (graph[xValue][yValue] == 1 && !visited[xValue][yValue]) {
+                    visited[xValue][yValue] = true;
+                    queue.push(new Node(xValue, yValue));
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private static class Node {
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+}
