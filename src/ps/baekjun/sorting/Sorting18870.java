cacce1fb781fff01123fa08cab7ca45e2716d98f
thom-window <hsbang.thom@gmail.com>
2022-08-09 21:12:03 +0900

    boj(sorting): solve 18870. map and sort.

--- src/ps/baekjun/sorting/Sorting18870.java
+++ src/ps/baekjun/sorting/Sorting18870.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.HashMap;
 
 //Silver2
 public class Sorting18870 {
@@ -12,38 +12,31 @@ public class Sorting18870 {
     static int N;
     static int[] array;
 
+    static int[] sorted;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         array = new int[N];
-        final int[] array2;
         String[] strings = br.readLine().split(" ");
 
-        for (int i = 0; i < strings.length; i++) {
-            array[i] = Integer.parseInt(strings[i]);
-        }
+        array = Arrays.stream(strings).mapToInt(Integer::parseInt).toArray();
+        sorted = Arrays.stream(strings).distinct().mapToInt(Integer::parseInt).sorted().toArray();
 
+        HashMap<Integer, Integer> map = new HashMap<>();
         StringBuilder sb = new StringBuilder();
 
-        array2 = Arrays.stream(strings).distinct().mapToInt(Integer::parseInt).sorted().toArray();
-
-        for (int i = 0; i < N; i++) {
-
-            for (int j = 0; j < N; j++) {
-                if (i == j) {
-                    continue;
-                }
-
-                if (array[i] > array[j]) {
-
-                }
-            }
+        for (int i = 0; i < sorted.length; i++) {
+            map.put(sorted[i], i);
         }
 
-//        System.out.println(Arrays.toString(result));
+        for (int i : array) {
+            int rank = map.get(i);
+            sb.append(rank).append(" ");
+        }
 
+        System.out.println(sb);
     }
 
-//    private
 }
