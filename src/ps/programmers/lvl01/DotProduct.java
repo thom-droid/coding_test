467e81e93398fc192ae6575dde5191fac4f76b4a
thom-window <hsbang.thom@gmail.com>
2022-11-18 12:17:13 +0900

    daily

---
+++ src/ps/programmers/lvl01/DotProduct.java
@@ -0,0 +1,16 @@
+package ps.programmers.lvl01;
+
+public class DotProduct {
+
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+
+        return answer;
+    }
+
+
+}
