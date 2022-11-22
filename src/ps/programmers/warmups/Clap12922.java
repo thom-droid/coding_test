c4de6374953c0c400e0d6b91e6ff6c94452c8372
thom <hsbang.thom@gmail.com>
2022-11-22 16:22:38 +0900

    daily

---
+++ src/ps/programmers/warmups/Clap12922.java
@@ -0,0 +1,19 @@
+package ps.programmers.warmups;
+
+public class Clap12922 {
+
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                sb.append("수");
+            } else {
+                sb.append("박");
+            }
+        }
+
+        return sb.toString();
+    }
+
+}
