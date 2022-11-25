ab9020974fa431ead6c7f626dc3e5b5722d28694
thom-window <hsbang.thom@gmail.com>
2022-11-26 00:35:56 +0900

    daily - simul

--- src/ps/programmers/lvl02/Wordplay12981.java
+++ src/ps/programmers/lvl02/Wordplay12981.java
@@ -1,2 +1,40 @@
-package ps.programmers.lvl02;public class Wordplay12981 {
+package ps.programmers.lvl02;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class Wordplay12981 {
+
+    public int[] solution(int n, String[] words) {
+
+        HashMap<String, Boolean> wordStorage = new HashMap<>();
+        String endingChar = "";
+
+        for (int i = 0; i < words.length; i += n) {
+
+            for (int j = i; j < i + n; j++) {
+
+                if (wordStorage.get(words[j]) == Boolean.TRUE) {
+                    return new int[]{j % n + 1, i / n + 1};
+                }
+
+                if (endingChar.isEmpty()) {
+                    endingChar = words[j].substring(words[j].length() - 1);
+                    wordStorage.put(words[j], Boolean.TRUE);
+                    continue;
+                }
+
+                if (!words[j].startsWith(endingChar)) {
+                    return new int[]{j % n + 1, i / n + 1};
+                }
+
+                endingChar = words[j].substring(words[j].length() - 1);
+                wordStorage.put(words[j], Boolean.TRUE);
+
+            }
+
+        }
+
+        return new int[]{0, 0};
+    }
 }
