c2b33e5140b24a0dd463159d765f2ae590e892cd
thom <hsbang.thom@gmail.com>
2022-11-25 10:38:12 +0900

    daily - stack

---
+++ src/ps/programmers/lvl02/Parenthesis12909.java
@@ -0,0 +1,37 @@
+package ps.programmers.lvl02;
+
+import java.util.LinkedList;
+
+public class Parenthesis12909 {
+
+    public boolean solution(String s) {
+
+        if (s.startsWith(")")) {
+            return false;
+        }
+
+        if (s.length() % 2 == 1) {
+            return false;
+        }
+
+        LinkedList<String> stack = new LinkedList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            String parenthesis = String.valueOf(s.charAt(i));
+            if (parenthesis.equals("(")) {
+                stack.push(parenthesis);
+            } else {
+                if (!stack.isEmpty()) {
+                    String peek = stack.peek();
+                    if (peek.equals("(")) {
+                        stack.pop();
+                        continue;
+                    }
+                }
+                stack.push(parenthesis);
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
