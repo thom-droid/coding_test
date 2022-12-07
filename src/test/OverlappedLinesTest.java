d71530fa88455bbb38d50abf14a682b5b19571aa
thom-window <hsbang.thom@gmail.com>
2022-12-08 07:35:24 +0900

    code test

--- src/test/OverlappedLinesTest.java
+++ src/test/OverlappedLinesTest.java
@@ -1,4 +1,18 @@
+package test;
+
+import org.junit.Test;
+import ps.programmers.warmups.OverlappedLines;
+
 import static org.junit.Assert.*;
+
 public class OverlappedLinesTest {
-  
+
+    OverlappedLines overlappedLines = new OverlappedLines();
+
+    @Test
+    public void solution2() {
+
+        int expected = 2;
+        int result = overlappedLines.solution2(new int[][]{{0, 1}, {2, 5}, {3, 9}});
+    }
 }
