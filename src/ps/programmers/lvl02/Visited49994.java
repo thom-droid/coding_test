da7ff5fc57fca1130f8c935affdcc071d6e0c2e4
thom-window <hsbang.thom@gmail.com>
2022-12-11 16:48:06 +0900

    wip - daily

---
+++ src/ps/programmers/lvl02/Visited49994.java
@@ -0,0 +1,16 @@
+package ps.programmers.lvl02;
+
+import java.util.HashMap;
+
+public class Visited49994 {
+
+    public int solution(String dirs) {
+        // 해시맵으로 중복 제거
+        HashMap<int[], Integer> map = new HashMap<>();
+
+        int answer = 0;
+        return answer;
+    }
+
+
+}
