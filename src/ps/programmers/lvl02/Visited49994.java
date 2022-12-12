9f519e4a74b83c98e1c011176259d03e449334a4
thom-window <hsbang.thom@gmail.com>
2022-12-12 11:03:48 +0900

    wip - 튜플 개념을 활용해서 다시 풀어볼 것

--- src/ps/programmers/lvl02/Visited49994.java
+++ src/ps/programmers/lvl02/Visited49994.java
@@ -4,13 +4,83 @@ import java.util.HashMap;
 
 public class Visited49994 {
 
-    public int solution(String dirs) {
-        // 해시맵으로 중복 제거
-        HashMap<int[], Integer> map = new HashMap<>();
+    private final int[] pointer = new int[]{0, 0};
+    private final HashMap<String, Integer> map = new HashMap<>();
 
-        int answer = 0;
-        return answer;
+    public int solution(String dirs) {
+
+        for (int i = 0; i < dirs.length(); i++) {
+            char instruction = dirs.charAt(i);
+            movePointer(instruction);
+        }
+
+        map.remove("duplicated");
+
+        return map.size();
     }
 
+    private void movePointer(char instruction) {
+
+        int x = pointer[1];
+        int y = pointer[0];
+        String record;
+        boolean instructed = false;
+
+        if (instruction == 'U' || instruction == 'D') {
+            instructed = instructedY(y, instruction);
+        }
+
+        if (instruction == 'R' || instruction == 'L') {
+            instructed = instructedX(x, instruction);
+        }
+
+        if (instructed) {
+            record = buildString(x, y);
+            map.put(record, 1);
+        }
+
+    }
+
+    private boolean instructedX(int x, char instruction) {
+
+        if (x + 5 <= 0 || x - 5 >= 10){
+            map.put("duplicated", 1);
+            return false;
+        }
+
+        if (instruction == 'L') {
+            pointer[1]--;
+        }
+
+        if (instruction == 'R') {
+            pointer[1]++;
+        }
+
+        return true;
+    }
+
+    private boolean instructedY(int y, char instruction) {
+
+        if (y + 5 <= 0 || y - 5 >= 10) {
+            map.put("duplicated", 1);
+            return false;
+        }
+
+        if (instruction == 'U') {
+            pointer[0]++;
+        }
+
+        if (instruction == 'D') {
+            pointer[0]--;
+        }
+
+        return true;
+    }
+
+    private String buildString(int x, int y) {
+
+        return "x : " + x + "to" + pointer[1] +
+                "y : " + y + "to" + pointer[0];
+    }
 
 }
---
+++ src/test/programmers/lvl2/Visited49994Test.java
@@ -0,0 +1,23 @@
+package test.programmers.lvl2;
+
+import org.junit.Test;
+import ps.programmers.lvl02.Visited49994;
+
+import static org.junit.Assert.*;
+
+public class Visited49994Test {
+
+    Visited49994 visited49994 = new Visited49994();
+
+    @Test
+    public void solution() {
+
+//        int expected = 7;
+//        int result = visited49994.solution("ULURRDLLU");
+
+        int expected2 = 7;
+        int result2 = visited49994.solution("LULLLLLLU");
+
+
+    }
+}
