097d0dd662e74412e698ab72d0f86ea9fbaafed7
thom-window <hsbang.thom@gmail.com>
2022-12-16 09:57:44 +0900

    sort with hashmap

--- src/ps/programmers/lvl02/TangerinePicker138476.java
+++ src/ps/programmers/lvl02/TangerinePicker138476.java
@@ -1,6 +1,9 @@
 package ps.programmers.lvl02;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
 
 public class TangerinePicker138476 {
 
@@ -37,6 +40,28 @@ public class TangerinePicker138476 {
         return answer;
     }
 
+    public int solution2(int k, int[] tangerine) {
+        int answer = 1;
+        HashMap<Integer, Integer> tangerines = new HashMap<>();
 
+        for (int i : tangerine) {
+            tangerines.put(i, tangerines.getOrDefault(i, 0) + 1);
+        }
+
+        ArrayList<Integer> list = new ArrayList<>(tangerines.keySet());
+
+        list.sort((o1, o2) -> tangerines.get(o2).compareTo(tangerines.get(o1)));
+
+        for (Integer key : list) {
+            if (k <= tangerines.get(key)) {
+                return answer;
+            }
+
+            k -= tangerines.get(key);
+            answer++;
+        }
+
+        return answer;
+    }
 
 }
--- src/test/programmers/lvl2/TangerinePicker138476Test.java
+++ src/test/programmers/lvl2/TangerinePicker138476Test.java
@@ -14,16 +14,23 @@ public class TangerinePicker138476Test {
 
         int expected = 3;
         int result = tangerinePicker138476.solution(6, new int[]{1, 3, 2, 5, 4, 5, 2, 3});
+        int resultA = tangerinePicker138476.solution2(6, new int[]{1, 3, 2, 5, 4, 5, 2, 3});
 
         int expected2 = 2;
         int result2 = tangerinePicker138476.solution(4, new int[]{1, 3, 2, 5, 4, 5, 2, 3});
+        int result2A = tangerinePicker138476.solution2(4, new int[]{1, 3, 2, 5, 4, 5, 2, 3});
 
         int expected3 = 1;
         int result3 = tangerinePicker138476.solution(2, new int[]{1, 1, 1, 1, 2, 2, 2, 3});
+        int result3A = tangerinePicker138476.solution2(2, new int[]{1, 1, 1, 1, 2, 2, 2, 3});
 
         assertEquals(expected, result);
         assertEquals(expected2, result2);
         assertEquals(expected3, result3);
+
+        assertEquals(expected, resultA);
+        assertEquals(expected2, result2A);
+        assertEquals(expected3, result3A);
 
     }
 }
