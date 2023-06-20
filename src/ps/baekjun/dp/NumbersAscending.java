bf4ecba75a07a3d6b1062348d4c61eeca48f93db
thom-window <hsbang.thom@gmail.com>
2023-06-20 11:59:57 +0900

    dp

--- src/ps/baekjun/dp/NumbersAscending.java
+++ src/ps/baekjun/dp/NumbersAscending.java
@@ -3,6 +3,7 @@ package ps.baekjun.dp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 //11057 silver 1
 public class NumbersAscending {
 
@@ -14,22 +15,23 @@ public class NumbersAscending {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        arr = new int[n+1][10];
+        arr = new int[n + 1][10];
 
-        for(int i = 0; i <= 9; i++){
+        for (int i = 0; i <= 9; i++) {
             arr[1][i] = 1;
         }
 
         System.out.println(dp(n));
+        System.out.println(dp2(n));
     }
 
-    public static long dp(int n){
+    public static long dp(int n) {
 
-        for(int i = 2; i <= n; i++){
+        for (int i = 2; i <= n; i++) {
 
-            for (int j = 0; j <= 9; j++){
+            for (int j = 0; j <= 9; j++) {
 
-                for(int k = 0; k <=j; k++){
+                for (int k = 0; k <= j; k++) {
 
                     arr[i][j] += arr[i - 1][k];
                 }
@@ -39,7 +41,28 @@ public class NumbersAscending {
 
         int result = 0;
 
-        for(int i = 0; i <=9; i++){
+        for (int i = 0; i <= 9; i++) {
+            result += arr[n][i];
+        }
+
+        return result % MOD;
+    }
+
+    public static long dp2(int n) {
+        for (int i = 0; i <= n; i++) {
+            arr[i][0] = 1;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= 9; j++) {
+                arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+                arr[i][j] %= MOD;
+            }
+        }
+
+        int result = 0;
+
+        for (int i = 0; i <= 9; i++) {
             result += arr[n][i];
         }
 
