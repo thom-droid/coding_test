f11f860fc4404090d11369d1261f9a15dfdc3fbc
thom-droid <thom-droid@github.com>
2024-03-06 22:39:51 +0900

    heap

---
+++ src/main/java/ps/programmers/lvl01/sort/heap/Feb0306_01.java
@@ -0,0 +1,18 @@
+package ps.programmers.lvl01.sort.heap;
+
+import java.util.PriorityQueue;
+
+public class Feb0306_01 {
+    public int[] solution(int k, int[] score) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        int[] answer = new int[score.length];
+        for (int i = 0; i < score.length; i++) {
+            heap.add(score[i]);
+            if (i >= k) heap.poll();
+            answer[i] = heap.peek();
+        }
+        return answer;
+    }
+    // queue does not guarantee the order of the elements when trying to loop through it.
+    // only methods like peek(), poll(), or remove() guarantee to present the value in the order of the elements.
+}
---
+++ src/test/java/ps/programmers/lvl01/sort/heap/Feb0306_01Test.java
@@ -0,0 +1,28 @@
+package ps.programmers.lvl01.sort.heap;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Feb0306_01Test {
+
+    Feb0306_01 test = new Feb0306_01();
+
+    @Test
+    void solution() {
+        int k = 3;
+        int[] score = {10, 100, 20, 150, 1, 100, 200};
+        int[] expected = {10, 10, 10, 20, 20, 100, 100};
+        int[] actual = test.solution(k, score);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void solution2() {
+        int k = 4;
+        int[] score = {0, 300, 40, 300, 20, 70, 150, 50, 500, 1000};
+        int[] expected = {0, 0, 0, 0, 20, 40, 70, 70, 150, 300};
+        int[] actual = test.solution(k, score);
+        assertArrayEquals(expected, actual);
+    }
+}
