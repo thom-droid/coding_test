105686d06077ceb312200ee70f52aa4e22bb6221
thom-droid <thom-droid@github.com>
2024-03-11 22:29:34 +0900

    simulation, array - refactoring

--- src/main/java/ps/programmers/lvl01/simulation/Mar0311.java
+++ src/main/java/ps/programmers/lvl01/simulation/Mar0311.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 public class Mar0311 {
+
     public int solution(String[] friends, String[] gifts) {
 
-        Integer[][] graph = new Integer[friends.length][friends.length];
+        int[][] graph = new int[friends.length][friends.length];
         int[] giftIdx = new int[friends.length];
         int[] giftToReceive = new int[friends.length];
         HashMap<String, Integer> giftMap = new HashMap<>();
@@ -15,27 +16,14 @@ public class Mar0311 {
             giftMap.put(friends[i], i);
         }
 
-        for (int i = 0; i < friends.length; i++) {
-            for (int j = 0; j < friends.length; j++) {
-                if (i == j) graph[i][j] = null;
-                else graph[i][j] = 0;
-            }
-        }
-
         for (String s : gifts) {
             String[] gift = s.split(" ");
             int sender = giftMap.get(gift[0]);
             int receiver = giftMap.get(gift[1]);
+            if (sender == receiver) continue;
             graph[sender][receiver]++;
-        }
-
-        for (int i = 0; i < graph.length; i++) {
-            for (int j = 0; j < graph[i].length; j++) {
-                if (i == j) continue;
-                Integer friendIdx = giftMap.get(friends[i]);
-                giftIdx[friendIdx] += graph[i][j];
-                giftIdx[friendIdx] -= graph[j][i];
-            }
+            giftIdx[sender]++;
+            giftIdx[receiver]--;
         }
 
         for (int i = 0; i < graph.length; i++) {
@@ -43,8 +31,8 @@ public class Mar0311 {
                 if (i == j) continue;
                 Integer f1 = giftMap.get(friends[i]);
                 Integer f2 = giftMap.get(friends[j]);
-                Integer f1ToF2 = graph[i][j];
-                Integer f2ToF1 = graph[j][i];
+                int f1ToF2 = graph[i][j];
+                int f2ToF1 = graph[j][i];
                 if (f1ToF2 > f2ToF1) {
                     giftToReceive[f1]++;
                 } else if (f2ToF1 > f1ToF2) {
@@ -58,6 +46,6 @@ public class Mar0311 {
                 }
             }
         }
-        return Arrays.stream(giftToReceive).map(x -> x/2).max().orElse(0);
+        return Arrays.stream(giftToReceive).map(x -> x / 2).max().orElse(0);
     }
 }
